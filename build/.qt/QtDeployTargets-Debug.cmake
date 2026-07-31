4134afa9f6a43b3700d5fc2b371937dae91cf3bd
BadTonho <209966374+BadTonho@users.noreply.github.com>
2026-07-31 17:13:49 -0300

    Limpeza de seguranca e blindagem de arquivos antes da publicacao

--- build/.qt/QtDeployTargets-Debug.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_NeoVDownloader_FILE C:/Users/Admin/Desktop/ProjetosCode/projetos/Baixar/build/Debug/NeoVDownloader.exe)
-set(__QT_DEPLOY_TARGET_NeoVDownloader_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_NeoVDownloader_RUNTIME_DLLS C:/Qt/6.7.2/msvc2019_64/bin/Qt6Widgetsd.dll;C:/Qt/6.7.2/msvc2019_64/bin/Qt6Networkd.dll;C:/Qt/6.7.2/msvc2019_64/bin/Qt6Guid.dll;C:/Qt/6.7.2/msvc2019_64/bin/Qt6Cored.dll)
