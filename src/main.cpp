d6516d47b50578820f214452423fba867d382946
BadTonho <209966374+BadTonho@users.noreply.github.com>
2026-08-01 11:09:03 -0300

    feat: implement application icon support and automate asset deployment in CMake

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
 #include <QApplication>
+#include <QIcon>
+#include <QFile>
 #include "MainWindow.h"
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
+
+    if (QFile::exists("app_icon.png")) {
+        app.setWindowIcon(QIcon("app_icon.png"));
+    } else if (QFile::exists("app_icon.ico")) {
+        app.setWindowIcon(QIcon("app_icon.ico"));
+    }
 
     MainWindow window;
     window.show();
